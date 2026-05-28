ee24659610e4e4cad56647a5679c06dc97e1b49f
Akash Ghosh <ag9749380@gmail.com>
2026-05-28 14:08:28 +0530

    feat: implement admin settings page with banner upload and display functionality

--- client/src/components/auth/PublicOnlyLayout.tsx
+++ client/src/components/auth/PublicOnlyLayout.tsx
@@ -1,19 +1,24 @@
 import { useAuthStore } from "@/features/auth/store";
 import { useAuth } from "@clerk/react";
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
+import { Commonloader } from "../common/Loader";
 
 export function PublicOnlyLayout() {
   const { isLoaded, isSignedIn } = useAuth();
   const { isBootstrapped, status } = useAuthStore();
+  const location = useLocation();
 
   if (!isLoaded) null;
 
   if (isSignedIn && (!isBootstrapped || status === "loading")) {
-    return null;
+    return <Commonloader />;
   }
 
-  if (isSignedIn) {
-    return <Navigate to={"profile"} replace />;
+  if (
+    isSignedIn &&
+    (location.pathname === "/sign-in" || location.pathname === "sign-up")
+  ) {
+    return <Navigate to={"/"} replace />;
   }
 
   return <Outlet />;
